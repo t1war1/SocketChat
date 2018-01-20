8e26ed706c9d1dd2fd6b3c1c5d3004d0db5b5efe
Gaurav <tiwarigaurav1998@gmail.com>
2018-01-20 23:39:14 +0530

    Star background+loader+rooms

---
+++ public_static/js/index.js
@@ -0,0 +1,138 @@
+var socket=io();
+
+var loadStatus=0;
+
+        $(()=>{
+
+            setTimeout(()=>{
+                $('#loading').fadeOut(100);
+                var chatSection=$('#chatSection');
+                var msgBox=$('#message')
+                var sendBtn=$('#send');
+                var chatBox=$('#chat');
+
+                var initialMsg=msgBox.val();
+                var currMsg="";
+
+                var userbox= $('#UserDetails')
+                    userbox.show();
+                var roombox=$('#RoomDetails')
+
+                var usernamebox=$('#username');
+                // var loginBtn=$('#loginbtn');
+
+                sendBtn.click(()=>{
+                    socket.emit('msg',{
+                        message:msgBox.val()
+                    })
+                })
+
+                socket.on('msg',(data)=>{
+                    chatBox.append(`
+                    <li>${data.sender} : ${data.message}</li>
+                    `)
+                })
+
+
+                window.onkeydown= function (event) {
+                    // Auto-focus the current input     when a key is typed
+                    if (!(event.ctrlKey || event.metaKey || event.altKey)) {
+                        usernamebox.focus();$('#roomname').focus();
+                    }
+                    // When the client hits ENTER on their keyboard
+                    if (event.which === 13) {
+                        if(loadStatus==0) {
+                            loadStatus=1;
+                            username = usernamebox.val();
+                            socket.emit('login', {
+                                username: username
+                            });
+                        }
+
+                        else if(loadStatus==1)
+                        {
+                            loadStatus=2;
+                            let roomName=$('#roomname').val()
+                            socket.emit('room',{
+                                roomName:roomName
+                            })
+                        }
+                    }
+                };
+
+                socket.on('roomNotThere',()=>{
+                    let roomMessage=$('#roomMessage');
+                    roomMessage.html(`<p>Room doesn't exist. Wanna create it? <button id="yes">YES</button>  <button id="no">NO</button></p>`)
+                    $('#yes').click(()=>{
+                        roombox.fadeOut();
+                        $('#loading').show();
+                        socket.emit('room_',{
+                            roomName:$('#roomname').val()
+                        })
+
+                    })
+                    $('#no').click(()=>{
+                        roomMessage.fadeOut();
+                        $('#roomname').clear();
+                    })
+                })
+
+
+                socket.on('roomJoined',(data)=>{
+                    $('#loading').fadeOut();
+                    $('#chatSection').show();
+                    chatBox.append(`<li style="border-radius: 0;">There are ${data.strength}</li><li style="border-radius: 0;>${data.you} joined</li>`);
+                })
+
+                socket.on('logged_in',(data)=>{
+                    if(data.loginStatus===0)
+                    {
+                        loadStatus:0;
+                        alert('Username not available');
+                        return;
+                    }
+                    userbox.fadeOut(100);
+                    roombox.show();
+                })
+
+                socket.on('someJoined',(data)=>{
+                    chatBox.append(`<p>${data.joinedUser} joined</p>`)
+                })
+
+                socket.on('typing',(data)=>{
+                    if(a===0) {
+                        chatBox.append(`<p class="typing">${data.userTyping} is typing...</p>`)
+                        a=1
+                    }
+                })
+
+
+                socket.on('left',(data)=>{
+                    chatBox.append(`<p>${data.userLeft} left</p>`)
+                })
+
+                setInterval(()=>{
+                    currMsg=msgBox.val()
+                    if(currMsg==="" || currMsg===initialMsg) {
+                        socket.emit('noTyping');
+                    }
+                    else{
+                        initialMsg=currMsg;
+                        socket.emit('typing');
+                    }
+                },500)
+
+                socket.on('noTyping',(data)=>{
+                    $('.typing').remove();
+                    a=0;
+                })
+
+            },1000);
+
+        })
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const socketIo=require('socket.io');
 const path=require('path');
 
 var users={};
+var rooms={};
 var userlist=[];
+var roomList=[];
+
 const io=socketIo(server);
 const PORT=process.env.PORT || 2323 ;
 
@@ -13,6 +16,7 @@ io.on('connection',(socket)=>{
 
     console.log("User id : "+ socket.id);
 
+    //user login
     socket.on('login',(data)=>{
             if(userlist.indexOf(data.username)!==-1) {
                 socket.emit('logged_in',{
@@ -33,6 +37,45 @@ io.on('connection',(socket)=>{
 
     })
 
+    //room login
+    socket.on('room',(data)=>{
+        if(roomList.indexOf(data.roomName)!=-1)
+        socket.join(data.roomName,()=>{
+            if(rooms[data.roomName]!=undefined){
+                rooms[data.roomName].strength+=1;
+            }
+            else {
+                rooms[data.roomName]={
+                    strength:1
+                }
+            }
+            socket.emit('roomJoined',{
+                strength:rooms[data.roomName].strength,
+                you:users[socket.id]
+            })
+        })
+        else{
+            socket.emit('roomNotThere');
+        }
+    })
+    socket.on('room_',(data)=>{
+        socket.join(data.roomName,()=>{
+            if(rooms[data.roomName]!=undefined)
+            rooms[data.roomName].strength+=1;
+            else {
+                rooms[data.roomName] = {
+                    strength: 1
+                }
+            }
+            socket.emit('roomJoined',{
+                strength:rooms[data.roomName].strength,
+                you:users[socket.id]
+            })
+        });
+    })
+
+
+    //for message
     socket.on('msg',(data)=>{
 
         io.emit('msg',{
@@ -41,6 +84,8 @@ io.on('connection',(socket)=>{
         })
     })
 
+
+    //disconnection
     socket.on('disconnect',()=>{
         socket.broadcast.emit('left',{
             userLeft:users[socket.id]
@@ -48,10 +93,12 @@ io.on('connection',(socket)=>{
         console.log(`${users[socket.id]} left`)
     })
 
+    //virtual tyyping message
     socket.on('typing',(data)=>{
             userTyping:users[socket.id]
         });
 
+    //noTyping helper
     socket.on('noTyping',(data)=>{
         socket.broadcast.emit('noTyping');
     })
